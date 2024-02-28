b82c4176d0431cec09b8a4d0b4e57c1ab42e1dcf
Von-sledge <papaamoah568@gmail.com>
2024-02-28 14:50:08 +0000

    Add new task solutions

--- binary_trees.h
+++ binary_trees.h
@@ -1,9 +1,9 @@
-#ifndef BIN_H_
-#define BIN_H_
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -12,6 +12,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
     int n;
@@ -21,9 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
